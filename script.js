83f4c3a82c5fd192f211922b91465f03b9f81875
Sakshi <gangwani.sakshi15@gmail.com>
2024-06-17 00:20:14 +0530

    implementing js

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!!';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.number').textContent = 15;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value)
